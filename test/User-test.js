a54bda3e40eecccd698ac69a14edc6c2aeb5e405
Carlyle Douglas <cdouglascorp@gmail.com>
2020-05-12 16:57:08 -0600

    Add testing suite to test User.js properties

--- test/User-test.js
+++ test/User-test.js
@@ -1,3 +1,34 @@
-const User = require('.src/User')
+const chai = require('chai');
+const expect = chai.expect;
+const User = require('../src/User');
 
-describe()
+describe('User', function() {
+  var user;
+  beforeEach(function() {
+    user = new User({
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    });
+  });
+
+  it('should be a function', function() {
+    expect(User).to.be.a('function');
+    });
+
+    it('should be an instance of User', function() {
+      expect(user).to.be.an.instanceof(User);
+    });
+
+    it('should be able to have properties', function() {
+      expect(user.name).to.equal("Luisa Hane")
+    });
+});

dee831a2a7982b4613167bbdade78a1d06b13b90
Carlyle Douglas <cdouglascorp@gmail.com>
2020-05-12 17:48:44 -0600

    Add test to find average step goal and remove commented out lines

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -3,33 +3,6 @@ const expect = chai.expect;
 const UserRepository = require('../src/UserRepository')
 const User = require('../src/User');
 const userData = require('../data/users_fixtures')
-// [{
-//   "id": 1,
-//   "name": "Luisa Hane",
-//   "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-//   "email": "Diana.Hayes1@hotmail.com",
-//   "strideLength": 4.3,
-//   "dailyStepGoal": 10000,
-//   "friends": [
-//     16,
-//     4,
-//     8
-//   ]
-// },
-// {
-//   "id": 2,
-//   "name": "Jarvis Considine",
-//   "address": "30086 Kathryn Port, Ciceroland NE 07273",
-//   "email": "Dimitri.Bechtelar11@gmail.com",
-//   "strideLength": 4.5,
-//   "dailyStepGoal": 5000,
-//   "friends": [
-//     9,
-//     18,
-//     24,
-//     19
-//   ]
-// }];
 
 describe('UserRepository', function() {
   var userRepository;
@@ -39,6 +12,7 @@ describe('UserRepository', function() {
     });
     userRepository = new UserRepository(users);
   });
+
   it('should be a function', function() {
     expect(UserRepository).to.be.a('function');
   });
@@ -50,4 +24,8 @@ describe('UserRepository', function() {
   it('should be able to find user by id', function() {
     expect(userRepository.findUserData(2).name).to.deep.equal("Jarvis Considine");
   });
+
+  it('should be able to find the average step goal', function() {
+    expect(userRepository.findAvgStepGoal()).to.equal(7500)
+  })
 })

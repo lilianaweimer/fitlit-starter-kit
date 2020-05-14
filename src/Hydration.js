550e7b024b527811eac6712037290ccaf153b3fa
Liliana Weimer <lilianaweimer11@gmail.com>
2020-05-14 17:28:53 -0600

    Add method for calculating number of ounces consumed for a day

--- src/Hydration.js
+++ src/Hydration.js
@@ -18,14 +18,27 @@ class Hydration {
     }, 0) / numOfUserHydrations;
   }
 
-  calculateOuncesConsumedByDay() {
+  calculateOuncesConsumedByDay(date) {
+    const userOunces = hydrationData.filter(currentHydration => {
+      return currentHydration.date === date && currentHydration.userID === this.userID
+    });
 
+    return userOunces[0].numOunces;
   }
 
-  calculateWeeklyOuncesConsumed() {
-
-  }
-}
+//   calculateWeeklyOuncesConsumed(endDate) {
+// //     var tomorrow = new Date();
+// // tomorrow.setDate(tomorrow.getDate() + 1);
+//     const weekEndDate = new Date(endDate);
+//     return hydrationData.reduce((totalOunces,currentHydration) => {
+//       if (currentHydration.userID === this.userID && currentHydration.date === this.date) {
+//         totalOunces += currentHydration.numOunces;
+//       }
+//       return totalOunces;
+//     });
+//     this.calculateOuncesConsumedByDay(endDate);
+//   }
+// }
 
 if (typeof module !== 'undefined') {
   module.exports = Hydration;

c83bf277972cd5b574d78d78bbc5bbc1a4276f99
Carlyle Douglas <cdouglascorp@gmail.com>
2020-05-20 19:21:10 -0600

    Moved global variables to be scoped to their functions and removed unnecessary map on users

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,9 @@
-const welcomeBox = document.querySelector('.welcome-box');
-const userCard = document.querySelector('.user-card')
-const userStepGoal = document.querySelector('.user-step-goal')
-const userDailyHydration = document.querySelector('.user-daily-hydration');
-const userWeeklyHydration = document.querySelector('.user-weekly-hydration');
-const userDailySleep = document.querySelector('.user-daily-sleep');
-const userWeeklySleep = document.querySelector('.user-weekly-sleep');
-const userAvgSleep = document.querySelector('.user-daily-average-sleep');
-const nameHeader = document.querySelector('#name-header');
+const userRepo = require('../src/UserRepository')
 
-var users = userData.map(user => {
-  return new User(user)
-});
-userRepository = new UserRepository(userData);
+const userRepository = new UserRepository(userData);
+// var users = userData.map(user => {
+//   return new User(user)
+// });
 
 window.addEventListener('load', onLoadHandler)
 
@@ -27,6 +19,8 @@ function onLoadHandler() {
   loadDailySleepData(sleep)
   loadWeeklySleepData(sleep)
   loadAvgSleepData(sleep)
+  sleep.findHighestHoursSlept("2019/07/04")
+  // let something = new Activity(activityData, userData)
 }
 
 function getRandomIndex(array) {
@@ -34,34 +28,37 @@ function getRandomIndex(array) {
 }
 
 function loadUsercard(user) {
-    let userInfo = user;
-    userCard.innerText = `
-      Name: ${userInfo.name}
-      Address: ${userInfo.address}
-      Email: ${userInfo.email}
-    `;
+  const userCard = document.querySelector('.user-card')
+  let userInfo = user;
+  userCard.innerText = `Name: ${userInfo.name}
+    Address: ${userInfo.address}
+    Email: ${userInfo.email}
+  `;
 }
 
 function loadStepGoal(user) {
+  const userStepGoal = document.querySelector('.user-step-goal')
   let userGoal = user.dailyStepGoal;
   let avgGoal = userRepository.findAvgStepGoal();
-  userStepGoal.innerText = `
-    Your Step Goal: ${userGoal}
+  userStepGoal.innerText = `Your Step Goal: ${userGoal}
     Average Step Goal: ${avgGoal}
   `;
 }
 
 function loadDailyHydrationData(hydration) {
+  const userDailyHydration = document.querySelector('.user-daily-hydration');
   let userHydration = hydration.calculateOuncesConsumedByDay("2019/09/22");
   userDailyHydration.innerText = `Water Consumed Today: ${userHydration}oz`
 }
 
 function loadWeeklyHydrationData(hydration) {
+  const userWeeklyHydration = document.querySelector('.user-weekly-hydration');
   let userWeekHydration = hydration.calculateWeeklyOuncesConsumed("2019/09/22");
   userWeeklyHydration.innerText = `Water Consumed This Week: ${userWeekHydration}oz`
 }
 
 function loadDailySleepData(sleep) {
+  const userDailySleep = document.querySelector('.user-daily-sleep');
   let userDailySleepHrs = sleep.calculateDailyHoursSlept("2019/09/22");
   let userDailySleepQuality = sleep.calculateDailySleepQuality("2019/09/22")
   userDailySleep.innerText = `Hours Slept Today: ${userDailySleepHrs}hrs.
@@ -69,6 +66,7 @@ function loadDailySleepData(sleep) {
 }
 
 function loadWeeklySleepData(sleep) {
+  const userWeeklySleep = document.querySelector('.user-weekly-sleep');
   let userWeeklySleepHrs = sleep.calculateWeeklyHoursSlept("2019/09/22");
   let userWeeklySleepQuality = sleep.calculateWeeklySleepQuality("2019/09/22")
   userWeeklySleep.innerText = `Hours Slept This Week: ${Math.floor(userWeeklySleepHrs)}hrs.
@@ -76,6 +74,7 @@ function loadWeeklySleepData(sleep) {
 }
 
 function loadAvgSleepData(sleep) {
+  const userAvgSleep = document.querySelector('.user-daily-average-sleep');
   let userAvgSleepHrs = sleep.calculateAvgDailySleep("2019/09/22");
   let userAvgSleepQuality = sleep.calculateAvgDailySleepQuality("2019/09/22")
   userAvgSleep.innerText = `Average Hours Slept Today: ${Math.floor(userAvgSleepHrs)}hrs.
@@ -83,7 +82,10 @@ function loadAvgSleepData(sleep) {
 }
 
 function loadUsername(user) {
+  const nameHeader = document.querySelector('#name-header');
+  const welcomeBox = document.querySelector('.welcome-box');
   let firstName = user.name.split(' ')[0];
+  // console.log(user.findFirstName())
   //I couldn't figure out how to call our findFirstName here
   //I think we need to figure out why our user method disappeared
   //It seems like we're not really instantiating an instance of user
@@ -99,3 +101,7 @@ function findAllUsernames() {
 }
 
 findAllUsernames()
+
+if (typeof module !== 'undefined') {
+  module.exports = userData;
+}
